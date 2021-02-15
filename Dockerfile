6d4d0c8e25af8858a6f8a20539f791f42c7edca6
Audun Fauchald Strand <audun.fauchald.strand@nav.no>
2021-02-15 09:35:07 +0100

    add deploy to prod
    Co-authored-by: Gøran Berntsen <goran.berntsen@nav.no>

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ WORKDIR /usr/src/app
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
+EXPOSE 8080
 CMD [ "python", "./deploy-datapipeline.py" ]
--- deploy-datapipeline.py
+++ deploy-datapipeline.py
@@ -1,1 +1,15 @@
-print ("hello world")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/isReady')
+def isReady():
+    return "OK"
+
+@app.route('/isAlive')
+def isAlive():
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
+    print("hello worlds")
