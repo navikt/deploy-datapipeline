e290a73c0233fd58a94dac99b5d952de2300e1f7
Audun Fauchald Strand <audun.fauchald.strand@nav.no>
2021-02-15 13:23:33 +0100

    added query to influxdb
    Co-authored-by: Gøran Berntsen <goran.berntsen@nav.no>

--- deploy-datapipeline.py
+++ deploy-datapipeline.py
@@ -1,15 +1,35 @@
 from flask import Flask
+from influxdb import InfluxDBClient, DataFrameClient
+import pandas as pd
 
 app = Flask(__name__)
+
 
 @app.route('/isready')
 def isReady():
     return "OK"
 
+
 @app.route('/isalive')
 def isAlive():
     return "OK"
 
+
+def getdeploydatafrominflux():
+    client = DataFrameClient('influxdb.adeo.no', 8086, 'tmp123', 'tmp123', 'metrics')
+    query = """    
+    SELECT * 
+    FROM "nais.deployment" 
+    WHERE ("team" =~ /.+/ 
+    AND "team" != 'aura'
+    AND "rollout_status" = 'complete' 
+    AND "environment" = "production")
+    """
+    result = client.query(query)
+    df = pd.concat(result).reset_index()
+    print(df.head())
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
-    print("hello worlds")
+    getdeploydatafrominflux()
