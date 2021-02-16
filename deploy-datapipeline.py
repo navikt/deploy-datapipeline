a32f78f316c2f2f6e99f6cc3ef5b4ff99072989d
Audun Fauchald Strand <audun.fauchald.strand@nav.no>
2021-02-16 08:00:11 +0100

    new approach to threading

--- deploy-datapipeline.py
+++ deploy-datapipeline.py
@@ -2,8 +2,54 @@ from flask import Flask
 from influxdb import InfluxDBClient, DataFrameClient
 import pandas as pd
 import threading
+import atexit
 
-app = Flask(__name__)
+POOL_TIME = 5 #Seconds
+
+# thread handler
+yourThread = threading.Thread()
+
+def createApp():
+    app = Flask(__name__)
+
+    def interrupt():
+        global yourThread
+        yourThread.cancel()
+
+    def getdeploydatafrominflux():
+        print("starting thread")
+        global yourThread
+
+        client = DataFrameClient('influxdb.adeo.no', 8086, 'tmp123', 'tmp123', 'metrics')
+        query = """    
+            SELECT * 
+            FROM "nais.deployment" 
+            WHERE ("team" =~ /.+/ 
+            AND "team" != 'aura'
+            AND "rollout_status" = 'complete' 
+            AND "environment" = "production")
+        """
+        result = client.query(query)
+        df = pd.concat(result).reset_index()
+        print(df.head())
+        # Set the next thread to happen
+        # yourThread = threading.Timer(POOL_TIME, getdeploydatafrominflux(), ())
+        # yourThread.start()
+
+    def doStuffStart():
+        # Do initialisation stuff here
+        global yourThread
+        # Create your thread
+        yourThread = threading.Timer(POOL_TIME, getdeploydatafrominflux, ())
+        yourThread.start()
+
+    # Initiate
+    doStuffStart()
+    # When you kill Flask (SIGTERM), clear the trigger for the next thread
+    atexit.register(interrupt)
+    return app
+
+app = createApp()
 
 
 @app.route('/isready')
@@ -15,23 +61,6 @@ def isReady():
 def isAlive():
     return "OK"
 
-
-def getdeploydatafrominflux():
-    client = DataFrameClient('influxdb.adeo.no', 8086, 'tmp123', 'tmp123', 'metrics')
-    query = """    
-    SELECT * 
-    FROM "nais.deployment" 
-    WHERE ("team" =~ /.+/ 
-    AND "team" != 'aura'
-    AND "rollout_status" = 'complete' 
-    AND "environment" = "production")
-    """
-    result = client.query(query)
-    df = pd.concat(result).reset_index()
-    print(df.head())
-
-
 if __name__ == "__main__":
     threading.Thread(app.run(host='0.0.0.0', port=8080)).start()
-    print("test")
-    getdeploydatafrominflux()
+    print("started service")
