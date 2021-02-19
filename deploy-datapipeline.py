184be3deae2ecfc402efa1036c3d6fe8a28a63ca
Gøran Berntsen <goran.berntsen@nav.no>
2021-02-19 09:55:29 +0100

    first draft (untested)

--- deploy-datapipeline.py
+++ deploy-datapipeline.py
@@ -6,7 +6,7 @@ import threading
 import atexit
 import requests
 import logging
-from google.cloud import storage
+from google.cloud import storage, bigquery
 import datetime
 
 POOL_TIME = 5  # Seconds
@@ -41,6 +41,34 @@ class veradata():
         date = today.strftime("%b-%d-%Y")
         blob = bucket.blob(date+"deploys-vera.csv")
         blob.upload_from_string(str(bytes))
+
+
+class bq_write():
+
+    def write_vera_history_to_bq(self, csv_file):
+        client = bigquery.Client()
+        job_config = bigquery.LoadJobConfig(
+            schema=[
+                bigquery.SchemaField("environment", "STRING"),
+                bigquery.SchemaField("application", "STRING"),
+                bigquery.SchemaField("version", "STRING"),
+                bigquery.SchemaField("deployer", "STRING"),
+                bigquery.SchemaField("deployed_timestamp", "TIMESTAMP"),
+                bigquery.SchemaField("replaced_timestamp", "TIMESTAMP"),
+                bigquery.SchemaField("environmentClass", "STRING"),
+                bigquery.SchemaField("id", "STRING")
+            ],
+            skip_leading_rows=1,
+            source_format=bigquery.SourceFormat.CSV
+        )
+        source_uri = "gs://nais-analyse-prod/bigquery/deploys/deploys.csv"
+        table_id = "nais-analyse-prod-2dcc.deploys"
+        load_job = client.load_table_from_uri(source_uri, table_id, job_config=job_config)
+        load_job.result()
+
+        destination_table = client.get_table(table_id)  # Make an API request.
+        print("Loaded {} rows.".format(destination_table.num_rows))
+
 
 
 def createApp():
