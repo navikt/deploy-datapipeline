ca73dfa4c15360b13ace63831b9e6ae0a685c170
Audun Fauchald Strand <audun.fauchald.strand@nav.no>
2021-02-22 09:19:29 +0100

    started on terraform
    Co-authored-by: Gøran Berntsen <goran.berntsen@nav.no>

---
+++ bigquery/bigquery.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "gcs" {
+    bucket = "nais-analyse-tfstate"
+  }
+}
+
+provider "google" {
+  version = "3.45.0"
+  project = "nais-analyse-prod"
+  region  = "europe-north1"
+}
+
+
+resource "google_project_service" "service" {
+  for_each = toset([
+    "cloudscheduler.googleapis.com",
+    "bigquery.googleapis.com",
+    "cloudfunctions.googleapis.com",
+    "compute.googleapis.com"
+  ])
+  service                    = each.key
+  disable_dependent_services = false
+}
