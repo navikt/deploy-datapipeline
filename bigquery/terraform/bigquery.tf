78dc8a7d442d283b5d45c3f3feda55ea8774716b
Audun Fauchald Strand <audun.fauchald.strand@nav.no>
2021-03-01 13:17:10 +0100

    trigger deploy

--- bigquery/terraform/bigquery.tf
+++ bigquery/terraform/bigquery.tf
@@ -18,6 +18,6 @@ resource "google_project_service" "service" {
     "cloudfunctions.googleapis.com",
     "compute.googleapis.com"
   ])
-  service                    = each.key
-  disable_dependent_services = false 
+  service                    = each.key 
+  disable_dependent_services = false
 }
