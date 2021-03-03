0eb4d6db425c3a99fb98bd2e49a608547dbf3d91
Audun Fauchald Strand <audun.fauchald.strand@nav.no>
2021-03-03 14:37:26 +0100

    made the code even more prettier
    Co-authored-by: Gøran Berntsen <goran.berntsen@nav.no>
    Co-authored-by: Truls Jørgensen <truls.jorgensen@nav.no>

--- deploy-datapipeline.py
+++ deploy-datapipeline.py
@@ -23,50 +23,33 @@ logger.addHandler(ch)
 class veradata():
 
     def getdeploydatafromvera(self):
-        logger.info("get data from vera")
         start = time.time()
         response = requests.get("https://vera.nais.oera.no/api/v1/deploylog?environment=p&csv=true")
-        # response = requests.get("https://vera.adeo.no/api/v1/deploylog?environment=p&csv=true")
-        end = time.time()
-        logger.info("vera.time " + str(end - start) + " seconds. ")
-        logger.info("vera.size " + str(len(response.content)) + " bytes. ")
-        return response.content
+        logger.info("vera: Time: {} seconds, size {}".format(str(time.time() - start)), str(len(response.content)))
+        return str(response.content, encoding="utf-8")
 
-    def writecodetobucket(self, bytes):
+    def writecodetobucket(self, file_as_string):
         client = storage.Client()
         bucket = client.get_bucket("deployments-vera")
-        today = datetime.date.today()
-        date = today.strftime("%b-%d-%Y")
-        blob_name = date + "deploys-vera.csv"
-        blob = bucket.blob(blob_name)
-
-        blob.upload_from_string(str(bytes, encoding="utf-8"), content_type="text/csv")
-
+        blob_name = datetime.date.today().strftime("%b-%d-%Y") + "-deploys-vera.csv"
+        bucket.blob(blob_name).upload_from_string(file_as_string, content_type="text/csv")
         return blob_name
 
     def write_vera_history_to_bq(self, filename):
         client = bigquery.Client(project="nais-analyse-prod-2dcc", location='europe-north1')
-        job_config = bigquery.LoadJobConfig(
-            skip_leading_rows=1,
-            source_format=bigquery.SourceFormat.CSV,
-            write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE
-        )
-
-        source_uri = "gs://deployments-vera/" + filename
-        logger.info(source_uri)
-        table_id = "nais-analyse-prod-2dcc.deploys.vera-deploys"
-        load_job = client.load_table_from_uri(source_uri, table_id, job_config=job_config)
+        load_job = \
+            client.load_table_from_uri(
+                "gs://deployments-vera/" + filename,
+                "nais-analyse-prod-2dcc.deploys.vera-deploys",
+                job_config=(bigquery.LoadJobConfig(
+                    skip_leading_rows=1,
+                    source_format=bigquery.SourceFormat.CSV,
+                    write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE
+                )))
 
         logger.info(f"Starting job {load_job.job_id} in project {load_job.project}")
-        logger.info("Details for job {} running in {}:".format(load_job.job_id, load_job.location))
-        logger.info(
-            "\tType: {}\n\tState: {}\n\tCreated: {}".format(
-                load_job.job_type, load_job.state, load_job.created
-            )
-        )
         load_job.result()
-
-        destination_table = client.get_table(table_id)  # Make an API request.
+        destination_table = client.get_table("nais-analyse-prod-2dcc.deploys.vera-deploys")
         logger.info("Loaded {} rows.".format(destination_table.num_rows))
 
 
