8d5af175e8ac811d74eefba9ade924b39441c3c8
Gøran Berntsen <goran.berntsen@nav.no>
2021-04-16 13:04:53 +0200

    schedule run every 24h

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
+import flask
 from flask import Flask
 import threading
+from datetime import datetime as dt
 from deploydataproduct import DeployDataProduct
 from datapakke import DeployDataPakke
 
 app = Flask(__name__)
-
+start_time = dt.now()
 
 @app.route('/isready')
 def isReady():
-    return "OK"
+    return flask.Response(status=200)
 
 
 @app.route('/isalive')
 def isAlive():
-    return "OK"
+    # Force k8s to restart the app every 24h as a super hacky scheduler for updating the data package
+    uptime = (dt.now() - start_time).total_seconds()
+    status = 500 if uptime > 60*60*24 else 200
+    return flask.Response(status=status)
 
 
 def run():
-
     produkt = DeployDataProduct()
-
     dataproduct_filename = produkt.run()
-
     pakke = DeployDataPakke()
     pakke.publiser_datapakke(dataproduct_filename)
 
