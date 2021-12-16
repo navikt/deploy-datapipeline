1f10fd75631df8b89b2860960d95275899b9fa09
Gøran Berntsen <goran.berntsen@nav.no>
2021-12-16 15:58:01 +0100

    write to /tmp/

--- datapakke.py
+++ datapakke.py
@@ -93,7 +93,7 @@ class DeployDataPakke:
         bucket = client.get_bucket(self.BUCKET_NAME)
 
         blob = bucket.get_blob(filename)
-        local_parquet_file = 'temp.parquet'
+        local_parquet_file = '/tmp/temp.parquet'
 
         with open(local_parquet_file, 'wb') as file:
             blob.download_to_file(file)
